2a1977960d69fe6b58dc5c8e97f3e85e2025cdbd
Sedov Andrey <sedov.andrey@bcg.com>
2018-06-25 11:00:33 +0300

    minor changes

--- bars.py
+++ bars.py
@@ -9,8 +9,10 @@ def load_data(filepath):
 
 def get_dict_bar(data):
     bar_dict = {}
+    bar_seats = 0
     for bar in data['features']:
-        bar_dict[bar['properties']['Attributes']['Name']] = bar['properties']['Attributes']['SeatsCount']
+        bar_seats = bar['properties']['Attributes']['SeatsCount']
+        bar_dict[bar['properties']['Attributes']['Name']] = bar_seats
     return bar_dict
 
 
@@ -30,13 +32,16 @@ def get_closest_bar(data, longitude, latitude):
     longitude_init = data['features'][0]['geometry']['coordinates'][0]
     latitude_init = data['features'][0]['geometry']['coordinates'][1]
     closest_bar_name = ''
-    distance_min = great_circle((latitude, longitude), (latitude_init, longitude_init)).meters
+    init_point = (latitude_init, longitude_init)
+    user_point = (latitude, longitude)
+    distance_min = great_circle(init_point, user_point).meters
 
     for bar in data['features']:
         bar_name = bar['properties']['Attributes']['Name']
         bar_longitude = bar['geometry']['coordinates'][0]
         bar_latitude = bar['geometry']['coordinates'][1]
-        distance = great_circle((bar_latitude, bar_longitude), (latitude, longitude)).meters
+        bar_point = (bar_latitude, bar_longitude)
+        distance = great_circle(bar_point, user_point).meters
         if distance < distance_min:
             distance_min = distance
             closest_bar_name = bar_name
@@ -52,6 +57,8 @@ if __name__ == '__main__':
     longitude = float(input('Enter longitude: '))
     latitude = float(input('Enter latitude: '))
     try:
-        print('The closest bar:', get_closest_bar(bar_data, longitude, latitude))
+        closest_bar = get_closest_bar(bar_data, longitude, latitude)
+        print('The closest bar:', closest_bar)
     except Exception:
         print('Coordinates are not valid')
+    exit()
